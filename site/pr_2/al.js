fbb975d5b2d0cbe40cd06b614663cdee1283bfef
UDQD <70804333+UDQD@users.noreply.github.com>
2020-10-14 11:10:31 +0300

    s

---
+++ site/pr_2/al.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert("asdsdgf\ndas");
+//dfffdfd
+/*sdfsdfsfd
+sadfsdfsfdsf
+sdffdsfs*/
+// alert("asdsdgf\ndads");
+const a = 123;
+var admin;
+var name = "Vasya";
+admin = name;
+var IIIIiiits_our_planet = "Ears";
+alert(typeof(admin));
